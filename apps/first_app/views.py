243dedb90ccb1fbf7e5eb5eefbe9d82fbc6d7dc2
Barkha <barkha0292@yahoo.com>
2017-10-28 13:35:59 -0700

    first commit

---
+++ apps/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse, redirect
+  # the index function is called when root is visited
+
+# Create your views here.
+
+def index(request):
+    response = "Hello, I am your first request!"
+    return HttpResponse(response)
+
